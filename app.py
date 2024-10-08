b108d272245d795566349dde9f65504374cdec05
Korulein <107486718+Korulein@users.noreply.github.com>
2024-10-08 12:18:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify, render_template
+import time
+import datetime
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 
+# Main page to fetch the links of all other pages
 @app.route('/')
 def linker():
     return "linker.html"
 
+# Render Template Examples
 @app.route('/Minerva')
 def minerva():
     return render_template("namer.html", person_name = "Minerva")
 
 @app.route('/Falacer')
 def falacer():
-    return render_template("namer.html", person_name = "Falacer")
+    return render_template("namer.html", person_name = "Falacer"
+                           )
+@app.route("/Time")
+def time_page():
+    return render_template('Time.html')
+
+# API route to return the current time as JSON
+@app.route("/get-time")
+def get_time():
+    current_time = timedata()
+    return jsonify(time1=current_time)
+
+# Function to get the current time
+def timedata():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return current_time
+
